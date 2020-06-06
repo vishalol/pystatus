0c2033afd2eff9ddcf6e7c9b9f0edd967ffabbe5
Vishal Sharma <vishaliitr7@gmail.com>
2020-06-06 13:53:06 +0530

    Add doc string to functions

--- status.py
+++ status.py
@@ -9,10 +9,13 @@ class GithubClient:
     repo = None
 
     def __init__(self, repository):
+        """init the github client and get the repository"""
+
         self.client = Github(GITHUB_TOKEN)
         self.repo = self.client.get_repo(repository)
 
     def post_status(self, commit_sha):
+        """ Post status for a github commit """
         res = self.repo.get_commit(sha=commit_sha).create_status(
             state="success",
             target_url="https://FooCI.com",
@@ -20,15 +23,14 @@ class GithubClient:
             context="ci/FooCI",
         )
         print("successfully posted")
-        print(res)
 
     def get_status(self, commit_sha):
+        """ Get combined status for a github commit """
         response = self.repo.get_commit(sha=commit_sha).get_combined_status()
         print(response)
 
 
 if __name__ == "__main__":
-
     argparser = argparse.ArgumentParser(add_help=False)
     argparser.add_argument("--sha", required=True, help="SHA of the commit")
     argparser.add_argument(
